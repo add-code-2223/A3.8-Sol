be1dab4605f618a65d03ecd7dca9b484e11f8fe8
María <frm@fernandowirtz.com>
2023-03-18 09:31:55 +0100

    serialversionUID

--- src/exceptions/InstanceNotFoundException.java
+++ src/exceptions/InstanceNotFoundException.java
@@ -2,6 +2,7 @@ package exceptions;
 
 public class InstanceNotFoundException extends Exception {
 
+	private static final long serialVersionUID = 5980816034634183939L;
 	private String className;
 
 	public InstanceNotFoundException(String className) {
